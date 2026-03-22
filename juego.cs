7f070f29071dae11c8e07e107f18092d948e581b
delacruzmercedeswilyerlinonasi-sudo <delacruzmercedeswilyerlinonasi@gmail.com>
2026-03-22 01:28:27 +0000

    Update console messages for game narrative

--- juego.cs
+++ juego.cs
@@ -4,10 +4,10 @@ class Programa
 {
     static void Main()
     {
-        Console.WriteLine("Estás solo en la oscuridad...");
+        Console.WriteLine("Estás solo y no tienes quien te ayude...");
         Console.Write("¿Escuchar sonido? (si/no): ");
         string r = Console.ReadLine();
-        if (r == "si") Console.WriteLine("Escuchas pasos detrás de ti...");
-        else Console.WriteLine("El silencio es peor...");
+        if (r == "si") Console.WriteLine("Escuchas pasos detrás de ti, felicidades ES UN DEMONIOO DE 2 CABEZAS...");
+        else Console.WriteLine("El silencio es bueno, PORQUE REVELA MAS FACIL TU FINAL...");
     }
 }
